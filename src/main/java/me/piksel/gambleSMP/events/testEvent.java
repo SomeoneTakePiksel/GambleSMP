faf833d5579f5f1064dff5367e2de54c4203ef40
SomeoneTakePiksel <aleksander.guziak@wp.pl>
2026-05-08 21:17:32 +0200

    Some new events

--- src/main/java/me/piksel/gambleSMP/events/testEvent.java
+++ src/main/java/me/piksel/gambleSMP/events/testEvent.java
@@ -2,8 +2,10 @@ package me.piksel.gambleSMP.events;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Random;
@@ -13,9 +15,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class testEvent {
     Random rand = new Random();
     final int min = 1;
-    final int max = 2;
+    final int max = 4;
     public void startEvent(Player player){
-        int value = (int) Math.floor(Math.random() * (max - min + 1) + min);
+        int value = (int) Math.floor(Math.random() * (max - min + 1) + min); //thx to someone that gave me this code :)
         if (value == 1){
             for (int i = 0; i < 7;i++){
                 player.getWorld().spawnEntity(player.getLocation(), EntityType.CREEPER);
@@ -26,6 +28,17 @@ public class testEvent {
             ItemStack d = new ItemStack(Material.DIAMOND,15);
             player.getWorld().dropItem(player.getLocation(),d);
             player.sendActionBar(ChatColor.AQUA + "Rich fella talk");
+            return;
+        } else if (value == 3) {
+            player.sendActionBar( ChatColor.ITALIC+""+ChatColor.GRAY +  "Nothing");
+            player.getWorld().spawnEntity(player.getLocation(), EntityType.BAT);
+            return;
+        } else if (value == 4) {
+            for (int i = 0; i < 20;i++){
+                player.getWorld().spawnEntity(player.getLocation(), EntityType.ZOMBIE);
+            }
+            player.sendActionBar(ChatColor.GREEN + "Me and my gng");
+            return;
         }
     }
 }
